693729065b16e0130b0627a7094a97cde4b6e773
Ryan Nair <ryannair05@gmail.com>
2024-03-16 17:21:03 -0400

    Add start and end date

--- FrontEndApp/CampusCal/CampusCal/CalendarView.swift
+++ FrontEndApp/CampusCal/CampusCal/CalendarView.swift
@@ -6,13 +6,15 @@
 //
 
 import SwiftUI
+import EventKit
 
 struct Event: Identifiable {
-    var id = UUID()
-    var title: String
-    var date: Date
-    var color: Color
-    var description: String
+    let id = UUID()
+    let title: String
+    let startDate: Date
+    let endDate: Date
+    let color: Color
+    let description: String
 }
 
 extension Date {
@@ -22,35 +24,36 @@ extension Date {
 }
 
 let sampleEvents: [Event] = [
-    Event(title: "Meeting with Alex", date: Date(), color: .blue, description: "Discuss project status."),
-    Event(title: "Lunch with Sam", date: Date().addingTimeInterval(3600 * 24), color: .green, description: "Casual lunch at the park."),
-    // Add more events with descriptions...
+    Event(title: "Meeting with Alex", startDate: Date(), endDate: Date().addingTimeInterval(3600), color: .blue, description: "Discuss project status."),
+    Event(title: "Lunch with Sam", startDate: Date().addingTimeInterval(3600 * 24), endDate: Date().addingTimeInterval((3600 * 24) + 3600), color: .green, description: "Casual lunch at the park."),
+    // Add more events...
 ]
 
 struct EventRow: View {
-    var event: Event
+    let event: Event
     
     var body: some View {
-        HStack {
-            Rectangle()
-                .fill(event.color)
-                .frame(width: 5)
-                .cornerRadius(2.5)
-            
-            VStack(alignment: .leading) {
-                Text(event.title)
-                    .font(.headline)
-                Text(event.date, format: .dateTime.hour().minute())
-                    .font(.subheadline)
-                Text(event.description)
-                    .font(.caption)
+        return NavigationLink {
+            Text(event.description)
+        } label: {
+            HStack {
+                Rectangle()
+                    .fill(event.color)
+                    .frame(width: 5)
+                    .cornerRadius(2.5)
+                
+                VStack(alignment: .leading) {
+                    Text(event.title)
+                        .font(.headline)
+                    Text(event.startDate...event.endDate)
+                        .font(.subheadline)
+                }
+                .padding(.leading, 8)
+                
+                Spacer()
             }
-            .padding(.leading, 8)
-            
-            Spacer()
+            .padding()
         }
-        .padding()
-//        .background(RoundedRectangle(cornerRadius: 10).fill(Color.white).shadow(radius: 1))
     }
 }
 
@@ -58,51 +61,70 @@ struct EventRow: View {
 struct CalendarView: View {
     var events: [Event]
     
-    // Group events by date
     private var groupedEvents: [Date: [Event]] {
-        Dictionary(grouping: events) { $0.date.startOfDay }
+        Dictionary(grouping: events) { $0.startDate.startOfDay }
     }
     
-    // Sorted dates for iteration
     private var sortedDates: [Date] {
         groupedEvents.keys.sorted()
     }
     
     var body: some View {
-        List {
-            ForEach(sortedDates, id: \.self) { date in
-                Section {
-                    ForEach(groupedEvents[date] ?? []) { event in
-                        EventRow(event: event)
-                            .swipeActions(edge: .leading, allowsFullSwipe: true) {
-                                Button {
-                                    print("Upvote \(event.title)")
-                                    // Implement upvote action here
-                                } label: {
-                                    Label("Upvote", systemImage: "arrow.up")
-                                }
-                                .tint(.orange)
-                                
-                                Button {
-                                    print("Downvote \(event.title)")
-                                    // Implement upvote action here
-                                } label: {
-                                    Label("Downvote", systemImage: "arrow.down")
-                                }
-                                .tint(.indigo)
+        NavigationStack {
+            List {
+                ForEach(sortedDates, id: \.self) { date in
+                    Section {
+                        if let events = groupedEvents[date] {
+                            ForEach(events) { event in
+                                EventRow(event: event)
+                                    .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                                        Button {
+                                            print("Upvote \(event.title)")
+                                            // Implement upvote action here
+                                        } label: {
+                                            Label("Upvote", systemImage: "arrow.up")
+                                        }
+                                        .tint(.orange)
+                                        
+                                        Button {
+                                            print("Downvote \(event.title)")
+                                            // Implement upvote action here
+                                        } label: {
+                                            Label("Downvote", systemImage: "arrow.down")
+                                        }
+                                        .tint(.indigo)
+                                    }
+                                    .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                                        Button {
+                                            let store = EKEventStore()
+                                            store.requestWriteOnlyAccessToEvents(completion: { _,_ in
+                                                let calEvent = EKEvent(eventStore: store)
+                                                calEvent.calendar = store.defaultCalendarForNewEvents
+                                                calEvent.title = event.title
+                                                calEvent.startDate = event.startDate
+                                                calEvent.endDate = event.endDate
+                                                calEvent.notes = event.description
+                                                
+                                                try? store.save(calEvent, span: .thisEvent)
+                                                
+                                            })
+                                        } label: {
+                                            Label("Add", systemImage: "plus")
+                                        }
+                                        .tint(.blue)
+                                    }
                             }
-                            .swipeActions(edge: .trailing, allowsFullSwipe: true) {
-                                Button {
-                                    print("Add \(event.title)")
-                                    // Implement add event action here Use alert toast here
-                                } label: {
-                                    Label("Add", systemImage: "plus")
-                                }
-                                .tint(.blue)
-                            }
+                        }
+                    } header: {
+                        Text(date.formatted(date: .abbreviated, time: .omitted))
                     }
-                } header: {
-                    Text(date.formatted(date: .abbreviated, time: .omitted))
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Add Class", systemImage: "calendar.badge.plus", action: {
+                        // to do
+                    })
                 }
             }
         }
