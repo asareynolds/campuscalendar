3ea17ee273a64f8ab7ce77d3448e7587ac4f7e5f
evaagunnn <140003676+evaagunnn@users.noreply.github.com>
2024-03-16 19:20:22 -0400

    AddEvents text area

--- FrontEndApp/CampusCal/CampusCal/CreateEventView.swift
+++ FrontEndApp/CampusCal/CampusCal/CreateEventView.swift
@@ -19,13 +19,13 @@ struct CreateEventView: View {
     @State private var title: String = ""
     @State private var startTime: Date = Date()
     @State private var endTime: Date = Date()
-    @State private var description: String = ""
+    @State private var description: String = "This event is to bring together people who love coding!...."
     @State private var url: String = ""
     @FocusState private var focusedField: Field?
     var body: some View {
         VStack(spacing: 10){
             Text("Create Event")
-                .padding(.bottom, 30)
+                .padding(.vertical, 25)
                 .font(.system(size:50))
             
             VStack {
@@ -52,8 +52,7 @@ struct CreateEventView: View {
                     displayedComponents: [.hourAndMinute]
                 )
                 
-                TextField(
-                    "Description",
+                TextEditor(
                     text: $description
                 )
                 .disableAutocorrection(true)
@@ -63,9 +62,19 @@ struct CreateEventView: View {
             }
             .textFieldStyle(.roundedBorder)
             .padding(.horizontal, 20)
+            Button("Submit") {
+
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 8))
+            .shadow(radius: 4)
+            .padding(.bottom, 20)
+            
+
+        }
+
         }
     }
-}
 #Preview {
     CreateEventView()
 }
